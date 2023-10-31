8ee016164e09341ab68f6a3a4e5d9e00992c19d2
harkred <harkeerat2003@gmail.com>
2023-10-31 16:16:53 -0600

    Added the edit profile feature that can  update the users' email, name and password

--- medicare/home/views.py
+++ medicare/home/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index_view(request):
     context = {
-
+        "err": None
     }
     return render(request, "index.html", context)
